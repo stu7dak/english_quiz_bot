f85d961dd0606a1f02f2272df948973fd394c8b2
stu7dak <migel42743@gmail.com>
2026-06-09 19:36:17 +0300

    Update topics.py

--- topics.py
+++ topics.py
@@ -1,1 +1,27 @@
-
+TOPICS = {
+    "present_simple": {
+        "title": "Present Simple",
+        "questions": [
+            {
+                "text": "She ___ to school every day.",
+                "options": ["go", "goes", "going", "gone"],
+                "correct": "goes"
+            },
+            {
+                "text": "___ they like pizza?",
+                "options": ["Do", "Does", "Is", "Are"],
+                "correct": "Do"
+            }
+        ]
+    },
+    "past_simple": {
+        "title": "Past Simple",
+        "questions": [
+            {
+                "text": "I ___ a movie yesterday.",
+                "options": ["watch", "watched", "watching", "watches"],
+                "correct": "watched"
+            }
+        ]
+    }
+}
