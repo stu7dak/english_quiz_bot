16e2517391e317a192b1c8fd3f26b5246250e301
stu7dak <migel42743@gmail.com>
2026-06-09 20:00:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 BOT_TOKEN = "8906119391:AAGtkFXAHd1y_En8Wdr_UM_z-WN9X41R300"
 
 # Имя вашего Google-документа (точно как в названии вкладки внизу таблицы, обычно "Лист1")
-SHEET_NAME = "Лист1"
+SHEET_NAME = "Quiz_Statistics"
 
 # Путь к скачанному JSON-файлу ключей Google (положите этот файл в папку с ботом)
 GOOGLE_CREDENTIALS_FILE = "credentials.json" 
