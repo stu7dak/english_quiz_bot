aebe86b74e422b68e803f60f4f312b885c5bea2c
stu7dak <migel42743@gmail.com>
2026-06-09 21:18:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get('8906119391:AAGtkFXAHd1y_En8Wdr_UM_z-WN9X41R300')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
 SHEET_NAME = os.environ.get('SHEET_NAME', 'Quiz_Statistics')
 GOOGLE_CREDENTIALS_FILE = 'credentials.json'
 MAX_ATTEMPTS = int(os.environ.get('MAX_ATTEMPTS', 3))
